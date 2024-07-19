7b8576f855d76f3b981dfb32e595bcdb885901ad
dscao <dscao.com@gmail.com>
2024-07-19 11:35:41 +0800

    fix for ha 2024.7+

--- custom_components/localtuya/common.py
+++ custom_components/localtuya/common.py
@@ -17,6 +17,7 @@ from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
+    dispatcher_send,
 )
 from homeassistant.helpers.restore_state import RestoreEntity
 
@@ -149,6 +150,7 @@ class TuyaDevice(pytuya.TuyaListener, pytuya.ContextualLogger):
                 float(self._config_entry[CONF_PROTOCOL_VERSION]),
                 self,
             )
+            
             self._interface.add_dps_to_request(self.dps_to_request)
 
             self.debug("Retrieving initial state")
@@ -234,13 +236,13 @@ class TuyaDevice(pytuya.TuyaListener, pytuya.ContextualLogger):
 
     def _dispatch_status(self):
         signal = f"localtuya_{self._config_entry[CONF_DEVICE_ID]}"
-        async_dispatcher_send(self._hass, signal, self._status)
+        dispatcher_send(self._hass, signal, self._status)
 
     @callback
     def disconnected(self):
         """Device disconnected."""
         signal = f"localtuya_{self._config_entry[CONF_DEVICE_ID]}"
-        async_dispatcher_send(self._hass, signal, None)
+        dispatcher_send(self._hass, signal, None)
         if self._unsub_interval is not None:
             self._unsub_interval()
             self._unsub_interval = None
